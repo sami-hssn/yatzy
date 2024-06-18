a6ce7433d2cf300b66947d4499ee7d69db04b570
Jack Snelgrove <jack.snelgrove@me.com>
2024-06-18 18:03:32 -0400

    changed stucture of js. Started to implement functionality

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
-    player='X';
-    const resetButton = document.getElementById('reset');
     const cells = document.querySelectorAll('.cell');
-    game= [
-        ['', '', ''], 
-        ['', '', ''], 
-        ['', '', '']]
+    var TicTacToe = (function(){
+        var game ={};
+        game.state=['', '', '', 
+                    '', '', '', 
+                    '', '', ''];
 
-    const takeTurn = (event) => {
-        const selectedCell = event.target;
-        const selectedCellIndex = parseInt(selectedCell.getAttribute('data-cell-index'));
-    
-    }
+        game.player='X';
+        game.winner='';
+
+        game.takeTurn = function(event){
+            const selectedCell = event.target;
+            const selectedCellIndex = parseInt(selectedCell.getAttribute('data-cell-index'));
+            game [selectedCellIndex] = game.player;
+            if (checkWin()){
+                announceWinner();
+            }
+            else if (checkDraw()){
+                announceDraw();
+            }
+            else{
+                changePlayer();
+            }
+            
+        }
+        game.resetGame= function(){
+            //Implement Reset
+        }
+
+        function changePlayer(){
+            if (this.player=='X'){
+                this.player='Y';
+            }
+            else{
+                this.player='X';
+            }
+        }
+        function checkWin(){
+            //Implement checkWin
+            return true;
+        }
+        function checkDraw(){
+            //Implement checkDraw
+            return false;
+        }
+
+        function announceWinner(){
+            if (this.player=='X'){
+                game.winner='X'
+            }
+            else{
+                this.winner='Y';
+            }
+            this.resetGame();
+        }
+        function announceDraw(){
+            //Implement announceDraw
+        }
+    return game;
+    }());   
 });
